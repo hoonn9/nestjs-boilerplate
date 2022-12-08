a4c1d1da49405f0e068373e08b44c51739c4eb9a
hoonn9 <hun9935@gmail.com>
2022-12-09 01:11:14 +0900

    test(CryptoService): update bcrypt and aes256

--- test/unit/core/crypto/crypto-service.spec.ts
+++ test/unit/core/crypto/crypto-service.spec.ts
@@ -14,15 +14,41 @@ describe('CryptoService', () => {
   });
 
   describe('AES256', () => {
-    it('should execute encrypt and decrypt', async () => {
-      const options = {
-        secret: faker.datatype.string(32),
-      };
+    const createOptionsByLen = (len: number) => ({
+      secret: faker.datatype.string(len),
+    });
+    it('Should execute encrypt and decrypt', async () => {
+      const options = createOptionsByLen(32);
+
       const plainText = faker.datatype.string(16);
       const encrypted = await cryptoService.encrypt(plainText, options);
       const decrypted = await cryptoService.decrypt(encrypted, options);
 
       expect(plainText).toEqual(decrypted);
+    });
+
+    it.each([1, 5, 12, 16, 24])(
+      'When key length is not 32, expect throw RangeError',
+      (len) => {
+        expect(
+          cryptoService.encrypt('', createOptionsByLen(len)),
+        ).rejects.toThrowError();
+      },
+    );
+
+    it('Should be key length is 32', () => {
+      expect(
+        cryptoService.encrypt('', createOptionsByLen(32)),
+      ).resolves.not.toThrow();
+    });
+  });
+
+  describe('Bcrypt', () => {
+    it('Compare Test', async () => {
+      const plainText = faker.datatype.string(5);
+      const encrypted = await cryptoService.hashByBcrypt(plainText, 10);
+      const isValid = await cryptoService.compareByBcrypt(plainText, encrypted);
+      expect(isValid).toBe(true);
     });
   });
 });
